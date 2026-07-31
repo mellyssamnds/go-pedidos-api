5b25519a67e463ca1794c1177e3cb5ebb0dff84c
mellyssamnds <mellyssah.mendes@live.com>
2026-07-30 21:18:31 -0300

    feat: adiciona erros conhecidos do domínio

---
+++ model/errors.go
@@ -0,0 +1,13 @@
+package model
+
+import "errors"
+
+var (
+	ErrClienteNaoEncontrado = errors.New("cliente não encontrado")
+	ErrProdutoNaoEncontrado = errors.New("produto não encontrado")
+	ErrPedidoNaoEncontrado  = errors.New("pedido não encontrado")
+
+	ErrEstoqueInsuficiente = errors.New("estoque insuficiente")
+	ErrEmailCadastrado     = errors.New("email já cadastrado")
+	ErrStatusInvalido      = errors.New("mudança de status inválida")
+)
