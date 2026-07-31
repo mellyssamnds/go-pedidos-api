dfa61915168464c2126fbb34a4e9c0678c777f55
mellyssamnds <mellyssah.mendes@live.com>
2026-07-31 01:39:36 -0300

    feat: adiciona erro ErrItemPedidoNaoEncontrado

--- model/errors.go
+++ model/errors.go
@@ -3,9 +3,10 @@ package model
 import "errors"
 
 var (
-	ErrClienteNaoEncontrado = errors.New("cliente não encontrado")
-	ErrProdutoNaoEncontrado = errors.New("produto não encontrado")
-	ErrPedidoNaoEncontrado  = errors.New("pedido não encontrado")
+	ErrClienteNaoEncontrado    = errors.New("cliente não encontrado")
+	ErrProdutoNaoEncontrado    = errors.New("produto não encontrado")
+	ErrPedidoNaoEncontrado     = errors.New("pedido não encontrado")
+	ErrItemPedidoNaoEncontrado = errors.New("item do pedido não encontrado")
 
 	ErrEstoqueInsuficiente = errors.New("estoque insuficiente")
 	ErrEmailCadastrado     = errors.New("email já cadastrado")
