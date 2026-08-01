29f446fba7fc22ac11c104fb7c9933bba0306532
mellyssamnds <mellyssah.mendes@live.com>
2026-08-01 00:19:22 -0300

    refactor: adiciona controllers, rotas, repositories e  services

--- main.go
+++ main.go
@@ -2,32 +2,54 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/mellyssamnds/go-pedidos-api/config"
+	"github.com/mellyssamnds/go-pedidos-api/controllers"
 	"github.com/mellyssamnds/go-pedidos-api/database"
+	"github.com/mellyssamnds/go-pedidos-api/repository"
+	"github.com/mellyssamnds/go-pedidos-api/routes"
+	"github.com/mellyssamnds/go-pedidos-api/service"
 )
 
 func main() {
-	// Carregar configuração
+	//carrega a configuração
 	cfg, err := config.Load()
-
 	if err != nil {
-		fmt.Printf("Erro ao carregar configuração: %v\n", err)
-		return
+		log.Fatalf("erro ao carregar configuração: %v", err)
 	}
 
-	// Conectar ao banco de dados
-	ctx := context.Background()
-	dsn := cfg.GetDSN()
-	dbPool, err := database.Connect(ctx, dsn)
-
+	//conecta banco de dados
+	pool, err := database.Connect(context.Background(), cfg.GetDSN())
 	if err != nil {
-		fmt.Printf("Erro ao conectar ao banco de dados: %v\n", err)
-		return
+		log.Fatalf("erro ao conectar ao banco de dados: %v", err)
 	}
+	defer pool.Close()
 
-	defer dbPool.Close()
+	log.Println("conexão com o banco de dados feita com sucesso!")
 
-	fmt.Println("Conexão com o banco de dados feita com sucesso!")
+	//repositories
+	clienteRepo := repository.NewPgClienteRepository(pool)
+	produtoRepo := repository.NewPgProdutoRepository(pool)
+	pedidoRepo := repository.NewPgPedidoRepository(pool)
+	itemPedidoRepo := repository.NewPgItemPedidoRepository(pool)
+
+	//services
+	clienteService := service.NewClienteService(clienteRepo)
+	produtoService := service.NewProdutoService(produtoRepo)
+	pedidoService := service.NewPedidoService(pedidoRepo, clienteRepo, produtoRepo, itemPedidoRepo, pool)
+
+	//controllers
+	clienteController := controllers.NewClienteController(clienteService)
+	produtoController := controllers.NewProdutoController(produtoService)
+	pedidoController := controllers.NewPedidoController(pedidoService)
+
+	//rotas
+	mux := routes.NewRouter(clienteController, produtoController, pedidoController)
+
+	log.Println("servidor rodando na porta 8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("erro ao iniciar servidor: %v", err)
+	}
 }
