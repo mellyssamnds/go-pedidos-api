6a50bf735172aaa4323cac842f30cd8631ff2a24
mellyssamnds <mellyssah.mendes@live.com>
2026-08-01 14:33:30 -0300

    refactor: adiciona mock do produto_repository

--- service/mocks_test.go
+++ service/mocks_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// mock ClienteRepository implementa mock ClienteRepository
 type MockClienteRepository struct {
 	mock.Mock
 }
@@ -30,4 +31,29 @@ func (m *MockClienteRepository) FindByEmail(ctx context.Context, email string) (
 func (m *MockClienteRepository) FindAll(ctx context.Context) ([]model.Cliente, error) {
 	args := m.Called(ctx)
 	return args.Get(0).([]model.Cliente), args.Error(1)
+}
+
+// mock ProdutoRepository implementa mock ProdutoRepository
+type MockProdutoRepository struct {
+	mock.Mock
+}
+
+func (m *MockProdutoRepository) Save(ctx context.Context, produto model.Produto) error {
+	args := m.Called(ctx, produto)
+	return args.Error(0)
+}
+
+func (m *MockProdutoRepository) FindByID(ctx context.Context, id uuid.UUID) (model.Produto, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(model.Produto), args.Error(1)
+}
+
+func (m *MockProdutoRepository) FindAll(ctx context.Context) ([]model.Produto, error) {
+	args := m.Called(ctx)
+	return args.Get(0).([]model.Produto), args.Error(1)
+}
+
+func (m *MockProdutoRepository) UpdateStock(ctx context.Context, id uuid.UUID, newQuantity int) error {
+	args := m.Called(ctx, id, newQuantity)
+	return args.Error(0)
 }
